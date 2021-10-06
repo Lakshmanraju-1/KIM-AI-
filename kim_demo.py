4161e8604c448740f8de4d29a99fe2cfe3a224aa
Lakshmanraju-1 <83155192+Lakshmanraju-1@users.noreply.github.com>
2021-10-06 22:42:42 +0530

    Add files via upload

---
+++ kim_demo.py
@@ -0,0 +1,131 @@
+import speech_recognition as sr # recognise speech
+import playsound # to play an audio files
+from gtts import gTTS # google text to speech
+import random
+from time import ctime # get time details
+import webbrowser # open browser
+import ssl
+import certifi
+import time
+import os # to remove created audio files
+from PIL import Image
+import subprocess
+import pyautogui #screenshot
+import pyttsx3
+import bs4 as bs
+import urllib.request
+
+class person:
+    name = ''
+    def setName(self, name):
+        self.name = name
+
+class asis:
+    name = ''
+    def setName(self, name):
+        self.name = name
+
+
+
+def there_exists(terms):
+    for term in terms:
+        if term in voice_data:
+            return True
+
+def engine_speak(text):
+    text = str(text)
+    engine.say(text)
+    engine.runAndWait()
+
+r = sr.Recognizer() # initialise a recogniser
+# listen for audio and convert it to text:
+def record_audio(ask=""):
+    with sr.Microphone() as source: # microphone as source
+        if ask:
+            engine_speak(ask)
+        audio = r.listen(source, 5, 5)  # listen for the audio via source
+        print("Done Listening")
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)  # convert audio to text
+        except sr.UnknownValueError: # error: recognizer does not understand
+            engine_speak('I did not get that')
+        except sr.RequestError:
+            engine_speak('Sorry, the service is down') # error: recognizer is not connected
+        print(">>", voice_data.lower()) # print what user said
+        return voice_data.lower()
+
+# get string and make a audio file to be played
+def engine_speak(audio_string):
+    audio_string = str(audio_string)
+    tts = gTTS(text=audio_string, lang='en') # text to speech(voice)
+    r = random.randint(1,20000000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file) # save as mp3
+    playsound.playsound(audio_file) # play the audio file
+    print(asis_obj.name + ":", audio_string) # print what app said
+    os.remove(audio_file) # remove audio file
+
+def respond(voice_data):
+    # 1: greeting
+    if there_exists(['hey','hi','hello']):
+        greetings = ["hey, how can I help you" + person_obj.name, "hey, what's up?" + person_obj.name, "I'm listening" + person_obj.name, "how can I help you?" + person_obj.name, "hello" + person_obj.name]
+        greet = greetings[random.randint(0,len(greetings)-1)]
+        engine_speak(greet)
+
+    # 2: name
+    if there_exists(["what is your name","what's your name","tell me your name"]):
+        if person_obj.name:
+            engine_speak("whats with my name ")
+        else:
+            engine_speak("i dont know my name . what's your name?")
+
+    if there_exists(["my name is"]):
+        person_name = voice_data.split("is")[-1].strip()
+        engine_speak("okay, i will remember that " + person_name)
+        person_obj.setName(person_name) # remember name in person object
+    
+    if there_exists(["your name should be"]):
+        asis_name = voice_data.split("be")[-1].strip()
+        engine_speak("okay, i will remember that my name is " + asis_name)
+        asis_obj.setName(asis_name) # remember name in asis object
+
+    # 3: greeting
+    if there_exists(["how are you","how are you doing"]):
+        engine_speak("I'm very well, thanks for asking " + person_obj.name)
+
+    #4: Amazon
+    if there_exists(["shopping","shop for","amazon"]):
+            search_term = voice_data.split("for")[-1]
+            url="https://www.amazon.in/"
+            webbrowser.get().open(url)
+            engine_speak(" here you go "+search_term + " on amazon ")
+             
+    if there_exists(["exit", "quit", "goodbye","bye",]):
+        engine_speak(["ok have a nice time ","Thank's for using me"])
+        exit()
+    #5 weather
+    if there_exists(["weather","tell me the weather report","whats the condition outside"]):
+        search_term = voice_data.split("for")[-1]
+        url = "https://www.google.com/search?sxsrf=ACYBGNSQwMLDByBwdVFIUCbQqya-ET7AAA%3A1578847393212&ei=oUwbXtbXDN-C4-EP-5u82AE&q=weather&oq=weather&gs_l=psy-ab.3..35i39i285i70i256j0i67l4j0i131i67j0i131j0i67l2j0.1630.4591..5475...1.2..2.322.1659.9j5j0j1......0....1..gws-wiz.....10..0i71j35i39j35i362i39._5eSPD47bv8&ved=0ahUKEwiWrJvwwP7mAhVfwTgGHfsNDxsQ4dUDCAs&uact=5"
+        webbrowser.get().open(url)
+        engine_speak("Here is what I found for on google")
+     # 6: search google
+    if there_exists(["search for"]) and 'youtube' not in voice_data:
+        search_term = voice_data.split("for")[-1]
+        url = "https://google.com/search?q=" + search_term
+        webbrowser.get().open(url)
+        engine_speak("Here is what I found for" + search_term + "on google")
+time.sleep(1)
+
+person_obj = person()
+asis_obj = asis()
+asis_obj.name = 'Kim'
+engine = pyttsx3.init()
+
+
+while(1):
+    voice_data = record_audio("Recording") # get the voice input
+    print("Done")
+    print("Q:", voice_data)
+    respond(voice_data) # respond
